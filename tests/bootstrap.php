d8b4bda54e80db09f3febee3c48bf80ada885c6d
ScorpioT1000 <scorpiot1000@yandex.ru>
2016-11-11 01:39:21 +0300

    tests upd

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,17 +16,15 @@ date_default_timezone_set('UTC');
 error_reporting(E_ALL | E_STRICT);
 
 use Doctrine\ORM\Tools\Setup;
-use Indaxia\OTR\Tests\Mocks\EntityManagerMock;
+use Indaxia\OTR\Tests\Mocks;
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
 $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/Entity"), $isDevMode);
 
-$entityManager = EntityManagerMock::create([
-    'driverClass'  => 'Indaxia\OTR\Tests\Mocks\DriverMock',
-    'wrapperClass' => 'Indaxia\OTR\Tests\Mocks\ConnectionMock',
-    'user'         => 'john',
-    'password'     => 'wayne'
-], $config);
+$entityManager = Mocks\EntityManagerMock::create(
+    new Mocks\ConnectionMock([], new Mocks\DriverMock()), 
+    $config
+);
 
 
