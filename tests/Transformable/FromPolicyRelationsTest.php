6554007c3f4490e49d59f066b9aebf5c636c1e44
ScorpioT1000 <scorpiot1000@yandex.ru>
2016-11-17 09:40:15 +0300

    tests upd

--- tests/Transformable/FromPolicyRelationsTest.php
+++ tests/Transformable/FromPolicyRelationsTest.php
@@ -64,7 +64,6 @@ class FromPolicyRelationsTest extends TestCase
         $this->assertNotEquals(null, $e->getManyB());
         $this->assertEquals(2, $e->getManyB()->count());
         $this->assertNotEquals(null, $e->getManyB()->get(0));
-        $this->assertNotEquals(null, $e->getManyB()->get(0)->getId());
         $this->assertEquals('new one', $e->getManyB()->get(0)->getValue());
         $this->assertNotEquals(null, $e->getManyB()->get(1));
         $this->assertEquals(20, $e->getManyB()->get(1)->getId());
