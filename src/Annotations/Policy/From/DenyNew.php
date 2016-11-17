143642426810aedbacc8042eed1b72db4194404e
ScorpioT1000 <scorpiot1000@yandex.ru>
2016-11-17 11:57:53 +0300

    hotfix

--- src/Annotations/Policy/From/DenyNew.php
+++ src/Annotations/Policy/From/DenyNew.php
@@ -13,7 +13,7 @@ use \Doctrine\ORM\Mapping as ORM;
  * @Annotation */
 class DenyNew
     extends \Indaxia\OTR\Annotations\Annotation
-    implements Interfaces\DenyInsertExistentFrom {
+    implements Interfaces\DenyNewFrom {
 
     public $priority = 0.6;
     public $propagating = false;
